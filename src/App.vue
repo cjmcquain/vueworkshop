96a9d760253a2c46ef58901eaaa196eabcb257a9
Charles McQuain <charlesmcquain@gmail.com>
2018-06-12 21:19:23 -0700

    Added v-if directive on e-mail address display

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,13 @@
   <div id="app">
     <h1>Hello there {{ user.firstname }} {{ user.lastname }}!</h1>
     <h2>You're logged in as {{ user.username }}</h2>
-    <h3>Your e-mail address is {{ user.email }}</h3>
+    <h3 v-if="myBoolean">Your e-mail address is {{ user.email }}</h3>
     Username: <input type="text" v-model="user.username"><br>
     First name: <input type="text" v-model="user.firstname"><br>
     Last name: <input type="text" v-model="user.lastname"><br>
     E-mail address: <input type="text" v-model="user.email"><br>
     Myboolean is {{ myBoolean }}
-    <button style="background-color: red" v-on:click="toggleBoolean">Show my info</button>
+    <button style="background-color: red" @click="toggleBoolean">Show my info</button>
   </div>
 </template>
 
